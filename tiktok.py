3f0b8673bd76c5e82ef812d87b4aed4a2a25cb5e
naturesbless <91281654+naturesbless@users.noreply.github.com>
2021-09-23 11:39:21 -0700

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -1,17 +1,15 @@
 from TikTokApi import TikTokApi
 
-uId = "6940061807843591000"
-secUid = "MS4wLjABAAAAvA6cxXvkBRrfcI7cQAW0eyZiT35H6XUiN_77y4PXOdKhMYfgAKfx7K2dKb2iIEZ1"
-verifyFp = "verify_ktswr95c_mSSa5qC2_mvku_4S0x_9Hnn_oCUJAqVl9fvg"
-
-
-user = "naturesbless"
-
+# Initialize the TikTok instance
 api = TikTokApi.get_instance()
-results = 10
 
-posts = api.user_posts(uId, secUid, count=1)
+# Ask for their username
+user = input("Enter your username: ")
 
-for tiktok in posts:
-    print('https://www.tiktok.com/@naturesbless/video/'+tiktok['video']['id'])
+# Get TikTok Dictionary for most recent (1) video
+userInfo = api.by_username(user, count=1)
+
+# Print direct link to most recent Tik Tok
+for tiktok in userInfo:
+    print('https://www.tiktok.com/@'+user+'/video/'+tiktok['id'])
 
