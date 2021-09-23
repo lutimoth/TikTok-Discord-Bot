38e08e07d05f05f55e1b36469d82b88ddfaca1a5
naturesbless <91281654+naturesbless@users.noreply.github.com>
2021-09-23 12:06:31 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,9 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from TikTokApi import TikTokApi
 
-#from TikTokApi import TikTokApi
-
-cookie = {
-  "s_v_web_id": "verify_ktue8vie_2Rzveakn_YpDn_4bu3_8PCj_AwsWhPeFj73l",
-  "tt_webid": "1%7CipXkLWzChYfRyMVw9HH2M1wpV3xsbysjMhft5Oa8iA4%7C1632266170%7C6e1a9ef8886a1d0823e95a9b49119220eb710b3eea005dc21d9176b5975b363d"
-}
+api = TikTokApi.get_instance()
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -17,17 +13,24 @@ intents = discord.Intents.default()
 
 bot = commands.Bot(command_prefix='!')
 
-#verifyFp = "verify_ktswr95c_mSSa5qC2_mvku_4S0x_9Hnn_oCUJAqVl9fvg"
-
-#tiktok = TikTokApi.get_instance(custom_verifyFp=verifyFp, use_test_endpoints = True)
-#user = "naturesbless"
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
+
 @bot.command(name='recent')
-async def message(ctx):
-    await ctx.send('this is working, sort of')
+async def _command(ctx):
+    global times_used
+    await ctx.send('Please input your TikTok Username')
+
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+
+    msg = await bot.wait_for('message', check=check)
+    userinfo = api.by_username(input(msg), count=1)
+    for tiktok in userinfo:
+        url = ('https://www.tiktok.com/@'+msg+'/video/'+tiktok['id'])
+    await ctx.send(url)
 
 bot.run(TOKEN)
