b26490efd5ca7b695ee46a1f7e465b03679695d2
naturesbless <91281654+naturesbless@users.noreply.github.com>
2021-09-23 11:22:23 -0700

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -1,15 +1,17 @@
 from TikTokApi import TikTokApi
 
+uId = "6940061807843591000"
+secUid = "MS4wLjABAAAAvA6cxXvkBRrfcI7cQAW0eyZiT35H6XUiN_77y4PXOdKhMYfgAKfx7K2dKb2iIEZ1"
 verifyFp = "verify_ktswr95c_mSSa5qC2_mvku_4S0x_9Hnn_oCUJAqVl9fvg"
+
 
 user = "naturesbless"
 
 api = TikTokApi.get_instance()
 results = 10
 
-trending = api.by_trending(count = results, custom_verifyFp = verifyFp)
+posts = api.user_posts(uId, secUid, count=1)
 
-for tiktok in trending:
-    print (tiktok['id'])
+for tiktok in posts:
+    print('https://www.tiktok.com/@naturesbless/video/'+tiktok['video']['id'])
 
-print(len(trending))
